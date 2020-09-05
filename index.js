af5ffd1d67fff9ce6a35a93c0725c90ceeccec01
213984 <53581675+213984@users.noreply.github.com>
2020-09-04 22:22:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8000
+app.all("*", function (req, res) {
+var fullUrl = "https" + '://' + req.get('host') + req.originalUrl;
+var url = new URL(fullUrl);
+var path = url.pathname
+res.send("hi")
+})
 
+app.listen(port)
