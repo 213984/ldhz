b87bd001be08549a9e230bea71bea6257e456358
213984 <53581675+213984@users.noreply.github.com>
2020-09-04 22:37:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 8000
-app.all("*", function (req, res) {
-var fullUrl = "https" + '://' + req.get('host') + req.originalUrl;
-var url = new URL(fullUrl);
-var path = url.pathname
-res.send("hi")
-})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(port)
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
