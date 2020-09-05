011e84d49deae7cda5e614003505a32f0de2160d
213984 <53581675+213984@users.noreply.github.com>
2020-09-04 22:48:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 8000
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () =>
+app.listen(2000, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
